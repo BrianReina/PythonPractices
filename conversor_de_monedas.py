ee74d821ba18c9ed44059f2ea8faec4423b10a8c
Brian Reina <brianreinaq@gmail.com>
2021-09-25 22:12:12 -0400

    Entry point borrado y Try and expect agregado

---
+++ conversor_de_monedas.py
@@ -0,0 +1,29 @@
+def conversor(tipo_pesos, valor_dolar):
+    pesos = float(input(f'¿Cuántos pesos {tipo_pesos} tienes?: '))
+    dolares = round(pesos / valor_dolar, 2)
+    print(f'Tienes {dolares} dólares')
+
+
+try:
+    menu = '''
+    Bienvenido al conversor de monedas 💰
+
+    1 - Pesos colombianos
+    2 - Pesos argentinos
+    3 - Pesos mexicanos
+
+    Elige una opción: '''
+
+    opcion = int(input(menu))
+
+    if opcion == 1:
+        conversor('colombianos', 3875)
+    elif opcion == 2:
+        conversor('argentinos', 65)
+    elif opcion == 3:
+        conversor('mexicanos', 24)
+    else:
+        print('Ingresa una opción correcta por favor')
+
+except ValueError:
+    print('¿Solo valores numericos!')
---
+++ peso_en_otro_planeta.py
@@ -0,0 +1,47 @@
+def run(dato, planeta):
+    GRAVEDAD = 9.807
+    peso = float(input('¿Cual es tu peso?: '))
+    ecuacion = round((peso / GRAVEDAD) * dato, 2)
+    print(f'Tu peso en {planeta} es: {ecuacion}kg')
+
+
+try:
+    menu = '''
+    Selecciona un numero para saber tu peso en otro Planeta
+
+        1- Mercurio
+        2- Venus
+        3- Marte
+        4- Júpiter
+        5- Saturno
+        6- Urano
+        7- Neptuno
+    '''
+    opcion = int(input(menu))
+
+    #Mercurio
+    if opcion == 1:
+        run(3.7, 'Mercurio')
+    #Venus
+    elif opcion == 2:
+        run(8.87, 'Venus')
+    #Marte
+    elif opcion == 3:
+        run(3.721, 'Marte')
+    #Júpiter
+    elif opcion == 4:
+        run(24.79, 'Júpiter')
+    #Saturno
+    elif opcion == 5:
+        run(10.44, 'Saturno')
+    #Urano
+    elif opcion == 6:
+        run(8.87, 'Urano')
+    #Neptuno
+    elif opcion == 7:
+        run(11.15, 'Neptuno')
+    else:
+        print('Ingresa una opcion correcta')
+
+except ValueError:
+    print('Solo se pueden ingresar numeros enteros')
