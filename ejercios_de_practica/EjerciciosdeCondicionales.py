10c22513aaf8fef8cf8c36aa0c2410bb318af945
Brian Reina <brianreinaq@gmail.com>
2021-10-19 11:20:28 -0400

    Nueva practica

---
+++ ejercios_de_practica/EjerciciosdeCondicionales.py
@@ -0,0 +1,12 @@
+# Ejercicio 1
+# Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+
+# age = int(input('Dime tu edad: '))
+# if age >= 18:
+#     print('¡Eres mayor de Edad!')
+# else:
+#     print('¡Eres menor de Edad!')
+
+
+# Ejercicio 2
+#Escribir un programa que almacene la cadena de caracteres contraseña en una variable, pregunte al usuario por la contraseña e imprima por pantalla si la contraseña introducida por el usuario coincide con la guardada en la variable sin tener en cuenta mayúsculas y minúsculas.
