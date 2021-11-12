9d28f18c1429039ffcdf8d113d02479170d09e69
Brian Reina <brianreinaq@gmail.com>
2021-11-12 18:56:31 -0400

    Start exercises of list and tuples

---
+++ ejercios_de_practica/EjerciciosdeListasYTuplas.py
@@ -0,0 +1,19 @@
+#Ejercicio 1
+#Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla.
+
+# asignature = ['Matematica', 'Fisica', 'Quimica', 'Historia', 'Lengua']
+# print(asignature)
+
+
+#Ejercicio 2
+#Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla el mensaje Yo estudio <asignatura>, donde <asignatura> es cada una de las asignaturas de la lista.
+
+# asignature = ['Matematica', 'Fisica', 'Quimica', 'Historia', 'Lengua']
+# print('Yo estudio:')
+# for i in asignature:
+#     print(i, '')
+
+
+#Ejercicio 3
+#Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista, pregunte al usuario la nota que ha sacado en cada asignatura, y después las muestre por pantalla con el mensaje En <asignatura> has sacado <nota> donde <asignatura> es cada una des las asignaturas de la lista y <nota> cada una de las correspondientes notas introducidas por el usuario.
+
