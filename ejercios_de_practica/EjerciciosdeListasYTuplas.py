0b7931107323069233fca76808f0732f6094dd88
Brian Reina <brianreinaq@gmail.com>
2021-11-15 20:15:00 -0400

    Completed exercises 4 to 6

--- ejercios_de_practica/EjerciciosdeListasYTuplas.py
+++ ejercios_de_practica/EjerciciosdeListasYTuplas.py
@@ -17,3 +17,47 @@
 #Ejercicio 3
 #Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista, pregunte al usuario la nota que ha sacado en cada asignatura, y después las muestre por pantalla con el mensaje En <asignatura> has sacado <nota> donde <asignatura> es cada una des las asignaturas de la lista y <nota> cada una de las correspondientes notas introducidas por el usuario.
 
+# asignatures = ['Matematica', 'Fisica', 'Quimica', 'Historia', 'Lengua']
+# nota = []
+# for asignature in asignatures:
+#     score = input(f'Nota obtenida en {asignature}: ')
+#     nota.append(score)
+# for i in range(len(asignatures)):
+#     print(f'La nota obtenida en {asignatures[i]}: {nota[i]}')
+
+
+#Ejercicio 4
+#Escribir un programa que pregunte al usuario los números ganadores de la lotería primitiva, los almacene en una lista y los muestre por pantalla ordenados de menor a mayor.
+
+# iteration = int(input('¿Cuantos numeros tiene la loteria primitiva?: '))
+# numbers = []
+# for i in range(iteration):
+#     data = int(input('Escribe los numeros ganadores, uno por uno: '))
+#     numbers.append(data)
+#     numbers.sort()
+# print(f'Los numeros ganadores son: {numbers}')
+
+
+#Ejercicio 5
+#Escribir un programa que almacene en una lista los números del 1 al 10 y los muestre por pantalla en orden inverso separados por comas.
+
+# elements = [1,2,3,4,5,6,7,8,9,10]
+# for i in range(1,11):
+#     print(elements[-i], end=', ')
+
+
+#Ejercicio 6
+#Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista, pregunte al usuario la nota que ha sacado en cada asignatura y elimine de la lista las asignaturas aprobadas. Al final el programa debe mostrar por pantalla las asignaturas que el usuario tiene que repetir.
+
+# asignatures = ['Matematica', 'Fisica', 'Quimica', 'Historia', 'Lengua']
+# user = []
+# for i in asignatures:
+#     note = int(input(f'Nota obtenida en {i}: '))
+#     if note < 5:
+#         user.append(i)
+    
+# print(f'Reprobaste {user}')
+
+
+#Ejercicio 7
+#Escribir un programa que almacene el abecedario en una lista, elimine de la lista las letras que ocupen posiciones múltiplos de 3, y muestre por pantalla la lista resultante.
